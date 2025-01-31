7775bbd7fdbd88ec60160469bf2d3dd02a39dc99
freedox-cts <alfre.cts@hotmail.com>
2025-01-31 01:03:42 -0300

    edita app.js 01

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let amigo = "";
+
+function agregarAmigo(){
+	amigo =documment.getElementById('amigo');
+
+	console.log(amigo);
+
+	if amigo != none {
+
+	}
+
+	amigos.add(amigo);
+
+}
+
+
+
+
